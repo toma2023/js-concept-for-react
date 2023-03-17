dea15e039e3c65247d97a17075afb577f0ca0f71
toma2023 <mrinmoyehimadrita@gmail.com>
2023-03-18 00:59:22 +0600

    js concept updated

---
+++ notation.js
@@ -0,0 +1,18 @@
+const person = {
+    name: 'lal nil holud mia', 
+    25: 'Summer',
+    profession: 'traffic surgeon', 
+    age: 39,
+    address: 'sohabag',
+    'son-name' : 'sunny'
+}
+// dot notation
+const prof1 = person.profession;
+//bracket notation
+const prof2 = person['profession'];
+const pName = 'profession';
+const prof3 = person[pName];
+const session = person[25];
+const son = person['son-name']
+console.log(son);
+
